779c9a48591a5f4113189c42bb0373c9378c1968
Ben Belchak <ben@needle.com>
2011-05-30 21:49:12 -0600

    Added some more commands to the pivotal tracker plugin.

--- campy/plugins/pivotal_tracker.py
+++ campy/plugins/pivotal_tracker.py
@@ -41,6 +41,8 @@ class PivotalTracker(CampyPlugin):
         help_text = """%s: Here is your help for the PivotalTracker plugin:
         pt story create "Title" "Description" bug|feature|chore|release -- Create a story
         pt getmine started|finished|delivered|accepted|rejected|unstarted|unscheduled -- Get a list of all stories that belong to you
+        pt start #story_id -- Start a particular story.
+        pt start|tell next bug|feature|chore -- Start or tell the next story of a given type.
         """ % speaker['user']['name']
         room.paste(help_text)
 
@@ -83,8 +85,34 @@ class PivotalTracker(CampyPlugin):
             else:
                 speak_text = "%s: You have no stories matching that query." % speaker['user']['name']
                 room.speak(speak_text)
+            return
+
+        m = re.match("%s: pt start #?(?P<id>\d+)$" % settings.CAMPFIRE_BOT_NAME, body)
+        if m:
+            story = tracker.GetStory(int(m.group('id')))
+            story.SetCurrentState('started')
+            story.SetOwnedBy(speaker['user']['name'])
+            tracker.UpdateStory(story)
+            room.speak("%s: [#%i] started by you! Get to work!" % (speaker['user']['name'],
+                                                    story.GetStoryId()))
+            return
+
+        m = re.match("%s: pt (?P<command>start|tell) next (?P<story_type>\w+)" % settings.CAMPFIRE_BOT_NAME, body)
+        if m:
+            next = tracker.GetStories("type:%s state:unstarted" % m.group('story_type'))[0]
+            if m.group('command') == 'start':
+                next.SetCurrentState('started')
+                next.SetOwnedBy(speaker['user']['name'])
+                tracker.UpdateStory(next)
+                room.speak("%s: [#%i] started by you! Get to work!" % (speaker['user']['name'],
+                                                                       next.GetStoryId()))
+            elif m.group('command') == 'tell':
+                room.speak("%s: [#%i] %s (%s)" % (settings.CAMPFIRE_BOT_NAME, next.GetStoryId(),
+                                                  next.GetName(), next.GetUrl()))
+            return
+
 
 
     def speak_new_story(self, room, new_story, speaker):
-        room.speak(unicode("%s: Your story has been created successfully."))
+        room.speak(unicode("%s: Your story has been created successfully." % speaker['user']['name']))
 
