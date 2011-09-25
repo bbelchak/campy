81ba9335c4c355e206b334318757e1f9d44be592
Zac Bowling <zac@zacbowling.com>
2011-09-25 15:42:21 -0700

    Updated zerocater changes

--- campy/plugins/zerocater.py
+++ campy/plugins/zerocater.py
@@ -1,10 +1,17 @@
+
+
 import urllib2, re
 from BeautifulSoup import BeautifulSoup
 from BeautifulSoup import BeautifulStoneSoup
+import sys, traceback, textwrap
 
+if __name__ == "__main__":
+    sys.path.append("../")
+    sys.path.append("./")
+    
 from plugins import CampyPlugin
 from campy import settings
-import sys, traceback
+
 
 class ZeroCater(CampyPlugin):
     def __init__(self, *args):
@@ -18,42 +25,69 @@ class ZeroCater(CampyPlugin):
         if not body.startswith(settings.CAMPFIRE_BOT_NAME):
             return
         
-        m = re.match('%s: zc$' % settings.CAMPFIRE_BOT_NAME, body)
+        m = re.match('%s: (?P<mode>zc|zcd)$' % settings.CAMPFIRE_BOT_NAME, body)
         if m:
             try:
-                #thar be horrible looking python below!
-                page = urllib2.urlopen(settings.ZERO_CATER_URL)
-                soup = BeautifulSoup(page)
-                paste_text = []
-                for week_box in soup.html.body.findAll("div",{"class":"week_box"}):
-                    paste_text.append(str(week_box.findPrevious("h2").renderContents()))
-                    meal_dates = week_box.findAll("div",{"class":"meal_date"})
-                    for meal_date in meal_dates: 
-                        meal_date_text = meal_date.getText().decode("UTF-8")
-                        meal_name = meal_date.findNext("div",{"class":"meal_name collapser-container"})
-                        time_name_size = meal_name.find("div",{"class":"grid_6 alpha"}).find("span")
-                        timetag = time_name_size.find("span",{"class":"collapser-controller"})
-                        timetag_text = str(timetag.find("span",{"class":"collapser-state "}).findNextSibling(text = True)).decode("UTF-8")
-                        food = timetag.findNextSibling(text = True)
-                        food_text = str(BeautifulStoneSoup(food.string,convertEntities=BeautifulStoneSoup.HTML_ENTITIES)).decode("UTF-8")
-                        restaurant = food.findNextSibling("a")
-                        restaurant_text = str(BeautifulStoneSoup(restaurant.string,convertEntities=BeautifulStoneSoup.HTML_ENTITIES)).decode("UTF-8").strip()
-                        people = restaurant.findNextSibling(text = True)
-                        people_text = str(BeautifulStoneSoup(people.string,convertEntities=BeautifulStoneSoup.HTML_ENTITIES)).decode("UTF-8")
-                        paste_text.append(u"\t %s @ %s - %s %s %s" % (meal_date_text.strip(), timetag_text.strip(), food_text.strip(), restaurant_text.strip(), people_text.strip()))
-                paste_text.append(u"\nMenu and voting: %s" % settings.ZERO_CATER_URL)
-                text = ('\n'.join(paste_text))
-                room.paste(text)
+                detail = True
+                if m.group('mode') == "zc":
+                    detail = False
+                room.paste(pull_zc(settings.ZERO_CATER_URL,detail))
             except Exception, e:
                 speak_text = u"%s: error parsing ZeroCater output." % speaker['user']['name']
                 room.speak(speak_text)
-                exc_type, exc_value, exc_traceback = sys.exc_info()
-                room.paste(repr(traceback.format_exception(exc_type, exc_value, exc_traceback)))
+                room.paste(traceback.format_exc())
                 
                 
 
     def send_help(self, campfire, room, message, speaker):
         help_text = """%s: Here is your help for the ZeroCater plugin:
         zc - display the zerocater menu
+        zcd - display the zerocater menu with item details and descriptions
         """ % speaker['user']['name']
         room.paste(help_text)
+        
+#thar be horrible looking python below!
+def pull_zc(pullurl,show_details=True):
+    page = urllib2.urlopen(pullurl)
+    soup = BeautifulSoup(page)
+    paste_text = []
+    for week_box in soup.html.body.findAll("div",{"class":"week_box"}):
+        paste_text.append(str(week_box.findPrevious("h2").renderContents()))
+        meal_dates = week_box.findAll("div",{"class":"meal_date"})
+        for meal_date in meal_dates: 
+            meal_date_text = meal_date.getText().decode("UTF-8")
+            meal_name = meal_date.findNext("div",{"class":"meal_name collapser-container"})
+            time_name_size = meal_name.find("div",{"class":"grid_6 alpha"}).find("span")
+            timetag = time_name_size.find("span",{"class":"collapser-controller"})
+            timetag_text = str(timetag.find("span",{"class":"collapser-state "}).findNextSibling(text = True)).decode("UTF-8")
+            food = timetag.findNextSibling(text = True)
+            food_text = str(BeautifulStoneSoup(food.string,convertEntities=BeautifulStoneSoup.HTML_ENTITIES)).decode("UTF-8")
+            restaurant = food.findNextSibling("a")
+            restaurant_text = str(BeautifulStoneSoup(restaurant.string,convertEntities=BeautifulStoneSoup.HTML_ENTITIES)).decode("UTF-8").strip()
+            people = restaurant.findNextSibling(text = True)
+            people_text = str(BeautifulStoneSoup(people.string,convertEntities=BeautifulStoneSoup.HTML_ENTITIES)).decode("UTF-8")
+            paste_text.append(u"  %s @ %s - %s %s %s" % (meal_date_text.strip(), timetag_text.strip(), food_text.strip(), restaurant_text.strip(), people_text.strip()))
+            
+            if show_details:
+                order_items = meal_name.find("ul",{"class":"order-summary"})
+            
+                for order_item in order_items.findAll("li"):
+                    order_item_text = str(order_item.contents[0]).decode("UTF-8")
+                    paste_text.append(u"    * %s" % order_item_text.strip())
+                    #paste_text.append(repr(order_item.contents).decode("UTF-8"))
+                    #print repr(order_item)
+                    order_item_description = order_item.find("div",{"class":"item-description"})
+                    if (order_item_description is not None):
+                        order_item_description = str(BeautifulStoneSoup(order_item_description.string,convertEntities=BeautifulStoneSoup.HTML_ENTITIES)).decode("UTF-8")
+                        order_item_description = textwrap.TextWrapper(initial_indent="      ",subsequent_indent="      ", width=60).fill(order_item_description)
+                        paste_text.append(order_item_description)
+                if (order_items is not None):
+                    paste_text.append("") #add a new line after the item list for asthetics
+            
+            
+            
+    paste_text.append(u"\nMenu and voting: %s" % pullurl)
+    return ('\n'.join(paste_text))
+    
+if __name__ == "__main__":
+    print pull_zc("http://www.zerocater.com/seatme",show_details=True)
